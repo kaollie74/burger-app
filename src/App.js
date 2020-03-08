396d4751d5fe3ca516e84d0259c725cd9d2b7669
kaollie74 <kaollie74@gmail.com>
2020-03-08 14:49:43 -0500

    created components and containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+// Components => stateless 
+import Layout from "./components/Layout/Layout";
+
+// Containers
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
-     
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     );
   }
