a1730d1adad2caeaa345cc6d2bbd6a340d1eb1dc
kaollie74 <kaollie74@gmail.com>
2020-03-09 19:58:09 -0500

    able to add type by onclick and display it in the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,30 @@
-import React from 'react'; 
-import classes from "./Burger.css"; 
+import React from 'react';
+import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
+import { array } from 'prop-types';
+
 const burger = (props) => {
+
+  // LOOK INTO THIS MORE
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((item) => {
+      return [...Array(props.ingredients[item])]
+        .map((_, i) => {
+          return <BurgerIngredient key={item + i} type={item} />
+        })
+    }).reduce((prevArray, currentItem) => {
+      return prevArray.concat(currentItem)
+    }, [])
+  console.log(transformedIngredients);
+
+if(transformedIngredients.length === 0){
+  transformedIngredients = <p>Please start adding ingredients</p>
+}
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,66 @@ import React, { Component } from 'react'
 
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }
+  // ADD INGREDIENT HANDLER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // type will either by 'salad', 'cheese', 'bacon', or 'meat.'
+  // this is how we will identify the changes since it will act like an index. 
+  addIngredientHandler = (type) => {
+    console.log('type: ', type);
+    // grab the current ingredient type that is in state
+    const oldCount = this.state.ingredients[type]
+    // updated that incrementing by 1
+    const updatedCount = oldCount + 1;
+    // grab state since its best to set state this way.
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    // update to the specific type that is in the copy state by
+    // the specific type.
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type]
+    console.log(priceAddition);
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    })
+  } // end addIngredientHandler 
+
+  removeIngredientHandler = (type) => {
+
+  }
+
   render() {
     return (
       <Aux>
-        <div><Burger/></div>
-        <div>Build Controls</div>
+        <div><Burger ingredients={this.state.ingredients} /></div>
+        <div>
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+          />
+        </div>
       </Aux>
     )
   }
