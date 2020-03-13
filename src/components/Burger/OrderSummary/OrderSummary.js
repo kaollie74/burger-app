8e885f4c93e8e29a7fa084b48b20e134b8892c42
kaollie74 <kaollie74@gmail.com>
2020-03-13 07:53:56 -0500

    added a modal and pass the properties of this.state.ingredients to orderSummary to display the values in the modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Aux from '../../../hoc/Aux';
+
+const OrderSummary = (props) => {
+  
+  // convert object into array of keys
+  // then map() through them to get the values. 
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map((item) => {
+      return (
+        <li key={item}>
+          <span style={{ textTransform: 'capitalize' }}>{item}</span>: {props.ingredients[item]}
+        </li>
+      )
+    });
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to checkout</p>
+    </Aux>
+  )
+}
+
+export default OrderSummary;
